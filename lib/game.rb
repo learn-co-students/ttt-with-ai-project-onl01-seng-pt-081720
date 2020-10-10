31697a1e7372aa98985cc375655185a623cab80e
J5WOOD <js.wood206@gmail.com>
2020-10-09 23:11:45 -0700

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,11 @@ class Game
   end
 
   def current_player
-
+    if self.board.turn_count % 2 == 0
+      player_1
+    else
+      player_2
+    end
   end
 
   def won?
@@ -35,10 +39,41 @@ class Game
   end
 
   def draw?
-    # binding.pry
-    if self.over? && !self.won?
+    self.over? && !self.won?
+  end
+
+  def winner
+    if self.won? != false
+      self.board.cells[self.won?[0]]
+    end  
+  end
+
+  def turn
+    turn = nil
+    while !self.board.valid_move?(turn)
+      turn = self.current_player.move(self.board)
+      if self.board.valid_move?(turn)
+        self.board.update(turn, self.current_player)
+        break
+      else
+        puts "Invalid selection, please enter selection again."
+      end
     end
   end
+
+  def play
+    until self.won? || self.draw?
+      self.turn
+    end
+    if self.won?
+      puts "Congratulations #{self.winner}!"
+    elsif self.draw?
+      puts "Cat's Game!"
+    end
+
+  end
+
+
   # provide basic runtime and logic
   # current player, won? winner
   #start play turn
